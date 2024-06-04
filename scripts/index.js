3a2a70c5dae44f9ecac32472bfae0dda9d348467
Saul Mijael Choquehuanca Huanca <saulchoque123@gmail.com>
2024-06-04 13:43:25 -0400

    Card grid fixed :)
    made with bootstrap lmao

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ var data = [
     {nombre: 'Tinte', profesion: 'Marca7', imagen: 'images/test/fedep.jpg'},
     {nombre: 'Secador', profesion: 'Marca8', imagen: 'images/test/fedep.jpg'},
     {nombre: 'Plancha', profesion: 'Marca9', imagen: 'images/test/fedep.jpg'},
-    
+
 
   // Más objetos aquí...
 ];
@@ -20,41 +20,47 @@ var container = document.getElementById('cardContainer');
 
 // Itera sobre tus datos
 for (var i = 0; i < data.length; i++) {
-  // Crea una nueva tarjeta
-  var card = document.createElement('div');
-  card.className = 'card';
+    // Crea una nueva c olumna
+    var col = document.createElement('div');
+    col.className = 'col-sm-3 my-3';
 
-  // Crea y añade la imagen
-  var img = document.createElement('img');
-  img.className = 'card-img-top';
-  img.src = data[i].imagen;
-  img.alt = 'Avatar';
-  img.style.width = '100%';
-  card.appendChild(img);
+    // Crea elemento Card
+    var card = document.createElement('div');
+    card.className = 'card';
 
-  // Crea y añade el contenedor de información
-  var infoContainer = document.createElement('div');
-  infoContainer.className = 'card-body';
-  card.appendChild(infoContainer);
+    // Crea y añade la imagen
+    var img = document.createElement('img');
+    img.className = 'card-img-top';
+    img.src = data[i].imagen;
+    img.alt = 'Avatar';
+    img.style.width = '100%';
+    card.appendChild(img);
 
-  // Crea y añade el nombre
-  var nom = document.createElement('h5');
-  nom.innerHTML = '<b>' + data[i].nombre + '</b>';
-  infoContainer.appendChild(nom);
+    // Crea y añade el contenedor de información
+    var infoContainer = document.createElement('div');
+    infoContainer.className = 'card-body';
+    card.appendChild(infoContainer);
 
-  // Crea y añade la profesión
-  var profession = document.createElement('p');
-  profession.className = 'card-text';
-  profession.textContent = data[i].profesion;
-  infoContainer.appendChild(profession);
+    // Crea y añade el nombre
+    var nom = document.createElement('h5');
+    nom.innerHTML = '<b>' + data[i].nombre + '</b>';
+    infoContainer.appendChild(nom);
+
+    // Crea y añade la profesión
+    var profession = document.createElement('p');
+    profession.className = 'card-text';
+    profession.textContent = data[i].profesion;
+    infoContainer.appendChild(profession);
 
 
-  var link = document.createElement('a');
-  link.className = 'btn btn-primary';
-  link.href = 'https://www.linkedin.com/in/federico-perez-9b6b7b1b4/';
-  link.innerHTML = 'Comprar';
-  infoContainer.appendChild(link);
+    // Filled Button Material Web (Boton)
+    var link = document.createElement('md-filled-button');
+    link.href = 'https://www.linkedin.com/in/federico-perez-9b6b7b1b4/';
+    link.target = '_blank';	
+    link.innerHTML = 'Comprar';
+    infoContainer.appendChild(link);
 
-  // Añade la tarjeta al contenedor
-  container.appendChild(card);
+    col.appendChild(card);
+    // Añade la tarjeta al contenedor
+    container.appendChild(col);
 }
