0430cf13b2ce0e2d09e2b74e138a90441e840521
Saul Mijael Choquehuanca Huanca <saulchoque123@gmail.com>
2024-06-04 12:26:14 -0400

    cards
    sisisi

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,17 @@
 
 // Supongamos que tus datos están en un array de objetos
 var data = [
-  {nombre: 'John Doe', profesion: 'Arquitecto & Diseñador', imagen: 'images/test/fedep.jpg'},
-  {nombre: 'Jane Doe', profesion: 'Ingeniera de Software', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Peine', profesion: 'Marca1', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Shampoo', profesion: 'Marca2', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Acondicionador', profesion: 'Marca3', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Crema', profesion: 'Marca4', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Gel', profesion: 'Marca5', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Cera', profesion: 'Marca6', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Tinte', profesion: 'Marca7', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Secador', profesion: 'Marca8', imagen: 'images/test/fedep.jpg'},
+    {nombre: 'Plancha', profesion: 'Marca9', imagen: 'images/test/fedep.jpg'},
+    
+
   // Más objetos aquí...
 ];
 
@@ -17,6 +26,7 @@ for (var i = 0; i < data.length; i++) {
 
   // Crea y añade la imagen
   var img = document.createElement('img');
+  img.className = 'card-img-top';
   img.src = data[i].imagen;
   img.alt = 'Avatar';
   img.style.width = '100%';
@@ -24,18 +34,26 @@ for (var i = 0; i < data.length; i++) {
 
   // Crea y añade el contenedor de información
   var infoContainer = document.createElement('div');
-  infoContainer.className = 'cardContainer';
+  infoContainer.className = 'card-body';
   card.appendChild(infoContainer);
 
   // Crea y añade el nombre
-  var nom = document.createElement('h4');
+  var nom = document.createElement('h5');
   nom.innerHTML = '<b>' + data[i].nombre + '</b>';
   infoContainer.appendChild(nom);
 
   // Crea y añade la profesión
   var profession = document.createElement('p');
+  profession.className = 'card-text';
   profession.textContent = data[i].profesion;
   infoContainer.appendChild(profession);
+
+
+  var link = document.createElement('a');
+  link.className = 'btn btn-primary';
+  link.href = 'https://www.linkedin.com/in/federico-perez-9b6b7b1b4/';
+  link.innerHTML = 'Comprar';
+  infoContainer.appendChild(link);
 
   // Añade la tarjeta al contenedor
   container.appendChild(card);
