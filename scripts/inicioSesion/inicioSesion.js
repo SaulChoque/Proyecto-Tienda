8001c66b66ce2ed13a22d0a3c6b9307213879e30
Saul Mijael Choquehuanca Huanca <schoquehuancah@fcpn.edu.bo>
2024-06-07 21:41:45 -0400

    Registration fixed Material Web

--- scripts/inicioSesion/inicioSesion.js
+++ scripts/inicioSesion/inicioSesion.js
@@ -1,44 +1,88 @@
-const registerForm = document.getElementById('register-form');
 
-registerForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  var email = document.getElementById('email');
-  var username = document.getElementById('username');
-  var password = document.getElementById('password').value;
-
-  const regexEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+\.)([a-zA-Z]{2,})$/;
-  if (!regexEmail.test(email.value)) {
-    email.error = 'El email no es válido';
-    var errorElement = document.createElement('div');
-    errorElement.textContent = email.error;
-    email.parentNode.appendChild(errorElement);
-    return;
-  }
-
-  if (username.value.length < 6) {
-    username.error='El nombre de usuario debe tener al menos 6 caracteres';
-    var errorElementUsername = document.createElement('div');
-    errorElementUsername.textContent = username.error;
-    username.parentNode.appendChild(errorElementUsername);
-    return;
-  }
+// Textfields
+var email = document.getElementById('email');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
 
 
-const regexPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_\W]).{8,}/;
-if (!regexPassword.test(password)) {
-    password.error = 'La contraseña debe tener al menos 8 caracteres, incluyendo una letra minúscula, una letra mayúscula, un número y un símbolo especial';
-    var errorElementPassword = document.createElement('div');
-    errorElementPassword.textContent = password.error;
-    password.parentNode.appendChild(errorElementPassword);
-    return;
+email.addEventListener('change', reportValidityEmail);
+username.addEventListener('change', reportValidityUsername);
+password.addEventListener('change', reportValidityPassword);	
+
+function reportValidityEmail(event) {
+  //console.log('reportValidity555');
+  const textField = event.target;
+  if (textField.reportValidity) {
+    const isValid = textField.reportValidity();
+    
+    if (!isValid) {
+      console.log('El campo no es válido');
+      // Manejar el caso cuando el campo no es válido
+      }
+      }
+      }
+function reportValidityUsername(event) {
+  //console.log('reportValidity555');
+  var errMessage='';
+  const textField = event.target;
+  if (textField.reportValidity) {
+    console.log(textField.value);
+    var isValid = textField.reportValidity();
+    if(textField.value.length < 6){
+      isValid = false;
+      errMessage='El nombre de usuario debe tener al menos 6 caracteres';
+      }
+    if (!isValid) {
+      username.setAttribute('error', true);
+      username.setAttribute('error-text', errMessage);
+      console.log('Error: '+errMessage);
+      // Manejar el caso cuando el campo no es válido
+      } else {
+        username.removeAttribute('error-text');
+      username.removeAttribute('error');
+      }
+      }
+}
+function reportValidityPassword(event) {
+  var errMessage='';
+  const regexPassword = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_\W]).{8,}/;
+  //console.log('reportValidity555');
+  const textField = event.target;
+  if (textField.reportValidity) {
+    var isValid = textField.reportValidity();
+    if (!regexPassword.test(textField.value)) {
+      isValid = false;
+      errMessage='La contraseña debe tener al menos 8 caracteres, incluyendo una letra minúscula, una letra mayúscula, un número y un símbolo especial';
+      }
+      if (!isValid) {
+        password.setAttribute('error', true);
+        password.setAttribute('error-text', errMessage);
+        console.log('Error: '+errMessage);
+        // Manejar el caso cuando el campo no es válido
+        } else {
+          password.removeAttribute('error-text');
+      password.removeAttribute('error');
+      }
 }
 
-  alert('Registro exitoso!');
-  window.location.href = '../index.html'; 
+}
+//Dialogs
+var dialogConfirm = document.getElementById('dialog-confirm');
+//Buttons
+var toggleButton = document.getElementById('password-toggle');
+var submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', async () => {
+  await dialogConfirm.show();
 });
 
-closeButton.addEventListener('click', async () => {
-  await dialog.close();
+
+toggleButton.addEventListener('click', async() => {
+  if (password.type === 'password') {
+    password.setAttribute('type', 'text');
+    } else {
+    password.setAttribute('type', 'password');
+
+  }
 });
 
